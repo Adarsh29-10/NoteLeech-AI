39cc2cf3d54caff6f7cbb31c15b906467c6002ab
Adarsh Raghuwanshi <aadiraghuwanshi29@gmail.com>
2025-09-05 00:47:04 +0530

    Basic Setup of React

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='font-bold text-7xl text-amber-400'>Shree Ganesh</h1>
+    </div>
+  );
+};
+
+export default App;
